0cd93cd48244a34ed022f909697d0763c8c2da58
Rizal <43487664+rizalarifnugraha@users.noreply.github.com>
2018-10-21 12:01:07 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan41/massajenis/Kubus.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan41.massajenis;
+
+/**
+ *
+ * @author acer
+ */
+public class Kubus {
+
+    private int sisi;
+    private int massa;
+
+    public int getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
+
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+
+    public int hitungVolume(int parSisi) {
+        return sisi * sisi * sisi;
+
+    }
+
+    public int hitungMassaJenis(int Massa, int volume) {
+        return Massa / volume;
+    }
+
+}
